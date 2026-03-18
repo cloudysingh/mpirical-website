fb97ff460aebc3a217bacb73d2b4be95c751faa3
cloudysingh <cloudy.singh@outlook.com>
2026-03-19 02:15:30 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+anchor.addEventListener("click", function(e) {
+e.preventDefault();
+
+document.querySelector(this.getAttribute("href"))
+.scrollIntoView({
+behavior:"smooth"
+});
+});
+});
+
+document.getElementById("contactForm").addEventListener("submit", function(e){
+e.preventDefault();
+alert("Thank you! We will contact you soon.");
+});
